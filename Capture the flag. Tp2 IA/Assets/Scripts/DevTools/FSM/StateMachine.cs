818868400b62c58fbf1cfa6e95670d8930fca1eb
Fran0W <frano.criscuolo@gmail.com>
2020-06-13 11:48:21 -0300

    BaseFrameWork

---
+++ Capture the flag. Tp2 IA/Assets/Scripts/DevTools/FSM/StateMachine.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine
+{
+    IState current;
+    IState previous;
+    
+    List<IState> stateList = new List<IState>();
+
+    public void Update()
+    {
+        if (current != null)
+        {
+            current.Tick();
+        }
+    }
+    public void Addstate(IState state)
+    {
+        stateList.Add(state);
+        if (current == null)
+        {
+            current = state;
+        }
+    }
+    public void ChangeState<T>() where T : IState
+    {
+        for (int i = 0; i < stateList.Count; i++)
+        {
+            if (stateList[i].GetType() == typeof(T))
+            {
+                current.Sleep();
+                current = stateList[i];
+                current.Start();
+            }
+        }
+    }
+    public bool IsActualState<T>() where T : IState
+    {
+        return current.GetType() == typeof(T);
+    }
+}
---
+++ Capture the flag. Tp2 IA/Assets/Scripts/Managers/Main.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    //Singleton
+    private static Main instance;
+    
+    
+    //Managers
+     
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if(instance == null)
+            instance = this;
+    }
+
+    
+}
